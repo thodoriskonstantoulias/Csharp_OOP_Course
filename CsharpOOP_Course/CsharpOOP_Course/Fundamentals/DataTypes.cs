482171eb2f8568698189846780a3c202ab00582c
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2020-04-17 21:46:13 +0300

    Fundamentals : Casting Data types

---
+++ CsharpOOP_Course/CsharpOOP_Course/Fundamentals/DataTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpOOP_Course.Fundamentals
+{
+    public class DataTypes
+    {
+        static int x = 0;
+        static float f = 1.5f;
+
+        public static void DifferenceInIntConversion()
+        {
+            Console.WriteLine("Value of float is " + f);
+            x = Convert.ToInt32(f);
+            Console.WriteLine("ConvertToInt32 method value : " + x);
+
+            x = (int)f;
+            Console.WriteLine("Type casting method value : " + x);
+        }
+    }
+}
---
+++ CsharpOOP_Course/CsharpOOP_Course/Program.cs
@@ -0,0 +1,17 @@
+﻿using CsharpOOP_Course.Fundamentals;
+using System;
+
+namespace CsharpOOP_Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fundamentals - 1 
+            //Tip : ConvertToInt32 actually rounds the number. Type casting just floors
+            DataTypes.DifferenceInIntConversion();
+
+            Console.ReadLine();
+        }
+    }
+}
